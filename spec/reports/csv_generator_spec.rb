3818ba4a9e4445db5357592799ef688e48b5bdcd
Shaun Muscat <shaun@intersect.org.au>
2017-05-12 17:29:42 +1000

    ANZARD-MISC: update csv generator spec to reflect site id needed and no abbrev

--- spec/reports/csv_generator_spec.rb
+++ spec/reports/csv_generator_spec.rb
@@ -2,49 +2,44 @@ require 'rails_helper'
 
 describe CsvGenerator do
   let(:survey) { create(:survey, name: "Survey One") }
-  let(:hospital) { create(:hospital, name: "Royal North Shore", abbrev: "RNS") }
+  let(:hospital) { create(:hospital, name: "Royal North Shore", unit: "IVF", site: "RNS") }
 
   describe "Generating the filename" do
 
     it "includes only survey name when no hospital or year of registration" do
-      expect(CsvGenerator.new(survey.id, "", "").csv_filename).to eq("survey_one.csv")
+      expect(CsvGenerator.new(survey.id, "", "", "").csv_filename).to eq("survey_one.csv")
     end
 
     it "includes survey name and hospital when hospital set" do
-      expect(CsvGenerator.new(survey.id, hospital.id, "").csv_filename).to eq("survey_one_rns.csv")
+      expect(CsvGenerator.new(survey.id, hospital.id, "", "").csv_filename).to eq("survey_one_rns.csv")
 
     end
 
     it "includes survey name and year of registration when year of registration set" do
-      expect(CsvGenerator.new(survey.id, "", "2009").csv_filename).to eq("survey_one_2009.csv")
+      expect(CsvGenerator.new(survey.id, "", "2009", "").csv_filename).to eq("survey_one_2009.csv")
 
     end
 
     it "includes survey name, hospital and year of registration when all are set" do
-      expect(CsvGenerator.new(survey.id, hospital.id, "2009").csv_filename).to eq("survey_one_rns_2009.csv")
+      expect(CsvGenerator.new(survey.id, hospital.id, "2009", "").csv_filename).to eq("survey_one_rns_2009.csv")
 
     end
 
     it "makes the survey name safe for use in a filename" do
       survey = create(:survey, name: "SurVey %/\#.()A,|")
-      expect(CsvGenerator.new(survey.id, "", "").csv_filename).to eq("survey_a.csv")
-    end
-
-    it "makes the hospital abbreviation safe for use in a filename" do
-      hospital = create(:hospital, abbrev: "HosPITAL %/\#.()A,|")
-      expect(CsvGenerator.new(survey.id, hospital.id, "").csv_filename).to eq("survey_one_hospital_a.csv")
+      expect(CsvGenerator.new(survey.id, "", "", "").csv_filename).to eq("survey_a.csv")
     end
   end
 
   describe "Checking for emptiness" do
     it "returns true if there's no matching records" do
-      expect(Response).to receive(:for_survey_hospital_and_year_of_registration).with(survey, hospital.id, "2009").and_return([])
-      expect(CsvGenerator.new(survey.id, hospital.id, "2009")).to be_empty
+      expect(Response).to receive(:for_survey_hospital_and_year_of_registration).with(survey, hospital.id, "2009", "").and_return([])
+      expect(CsvGenerator.new(survey.id, hospital.id, "2009", "")).to be_empty
     end
 
     it "returns false if there's matching records" do
       expect(Response).to receive(:for_survey_hospital_and_year_of_registration).and_return(["something"])
-      expect(CsvGenerator.new(survey.id, "", "")).not_to be_empty
+      expect(CsvGenerator.new(survey.id, "", "", "")).not_to be_empty
     end
   end
 
@@ -59,7 +54,7 @@ describe CsvGenerator do
       q_text = create(:question, section: section1, question_order: 2, question_type: Question::TYPE_TEXT, code: 'TextQ')
       q_time = create(:question, section: section1, question_order: 4, question_type: Question::TYPE_TIME, code: 'TimeQ')
 
-      response1 = create(:response, hospital: create(:hospital, abbrev: 'HRL'), survey: survey, year_of_registration: 2009, baby_code: 'ABC-123')
+      response1 = create(:response, hospital: create(:hospital, unit: 'IVF', site: 'HRL'), survey: survey, year_of_registration: 2009, baby_code: 'ABC-123')
       create(:answer, response: response1, question: q_choice, answer_value: '1')
       create(:answer, response: response1, question: q_date, answer_value: '25/02/2001')
       create(:answer, response: response1, question: q_decimal, answer_value: '15.5673')
@@ -69,14 +64,14 @@ describe CsvGenerator do
       response1.reload
       response1.save!
 
-      response2 = create(:response, hospital: create(:hospital, abbrev: 'BBB'), survey: survey, year_of_registration: 2011, baby_code: 'DEF-567')
+      response2 = create(:response, hospital: create(:hospital, unit: 'IVF', site: 'BBB'), survey: survey, year_of_registration: 2011, baby_code: 'DEF-567')
       create(:answer, response: response2, question: q_integer, answer_value: '99')
       create(:answer, response: response2, question: q_text, answer_value: 'ABCdefg Ijkl')
       response2.reload
       response2.save!
 
-      expect(Response).to receive(:for_survey_hospital_and_year_of_registration).with(survey, '', '').and_return([response1, response2])
-      csv = CsvGenerator.new(survey.id, '', '').csv
+      expect(Response).to receive(:for_survey_hospital_and_year_of_registration).with(survey, '', '', '').and_return([response1, response2])
+      csv = CsvGenerator.new(survey.id, '', '', '').csv
       expected = []
       expected << %w(RegistrationType YearOfRegistration Hospital CYCLE_ID ChoiceQ TextQ DateQ TimeQ IntegerQ DecimalQ)
       expected << ['Survey One', '2009', 'HRL', 'ABC-123', '1', 'ABc', '2001-02-25', '14:56', '877', '15.5673']
