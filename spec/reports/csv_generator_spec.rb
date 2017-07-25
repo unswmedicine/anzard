5df7bfd11dba273044360c1adf7648cb62f2dbb1
Shaun Muscat <shaun@intersect.org.au>
2017-07-25 11:20:37 +1000

    ANZARD-54: fix failing csv generator unit test

--- spec/reports/csv_generator_spec.rb
+++ spec/reports/csv_generator_spec.rb
@@ -36,8 +36,8 @@ describe CsvGenerator do
   describe "Checking for emptiness" do
     # ToDo: figure out why this test is failing with "RSpec::Mocks::MockExpectationError: #<Response(id: integer, survey_id: integer, user_id: integer, cycle_id: string, created_at: datetime, updated_at: datetime, clinic_id: integer, submitted_status: string, batch_file_id: integer, year_of_registration: integer, validation_status: string) (class)> received :for_survey_clinic_and_year_of_registration with unexpected arguments"
     it "returns true if there's no matching records" do
-      expect(Response).to receive(:for_survey_clinic_and_year_of_registration).with(survey, clinic.id, "", "2009").and_return([])
-      expect(CsvGenerator.new(survey.id, clinic.unit_code, "", "2009")).to be_empty
+      expect(Response).to receive(:for_survey_clinic_and_year_of_registration).with(survey, clinic.unit_code, clinic.site_code, '2009').and_return([])
+      expect(CsvGenerator.new(survey.id, clinic.unit_code, clinic.site_code, '2009')).to be_empty
     end
 
     it "returns false if there's matching records" do
