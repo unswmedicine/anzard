455c060e665a189cd4335832c1d53fa1b784225f
Shaun Muscat <shaun@intersect.org.au>
2019-09-05 13:25:56 +1000

    ANZARD-228: set home page, navigation and data form list
    - change home page to only contain navigational content
    - change navigation elements from tabs to placeholder links
    - create dedicated page for data entry form list/index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -19,9 +19,5 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home, raise: false
 
   def home
-    if user_signed_in?
-      set_tab :responses, :home
-      @responses = Response.accessible_by(current_ability).unsubmitted.order("cycle_id")
-    end
   end
 end
